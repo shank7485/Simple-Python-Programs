42188feb91e43d8d6e3bd1b66e042c6ab6f29ead
Shashank Kumar Shankar <shank7485@users.noreply.github.com>
2016-02-09 00:25:59 -0600

    Create PairChecker.py

---
+++ PairChecker.py
@@ -0,0 +1,15 @@
+lst = raw_input()
+
+lst = lst.split(", ")
+
+initial = 0
+
+for i in lst:
+    initial = int(i) ^ initial
+
+"""
+Idea is XOR of a number with itself is 0. So if I XOR all elements, it will turnup to 0 if all are pairs.
+Else, it will return the number which is without a pair.
+"""
+
+print initial
